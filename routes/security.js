0dd544835aba0c26205b45695d5cc5eb6ed2ba7c
Humberto6641 <hgmezmartnez@gamil.com>
2021-11-07 18:19:20 -0600

    make it better

--- routes/security.js
+++ routes/security.js
@@ -7,18 +7,18 @@ const jwt = require('jsonwebtoken');
 //Visualizar Personas
 router.post("/login", (req, res) => {
     const body = req.body;
-    console.log(body.userName);
+    console.log(body.nombre);
     let user;           
 
-    mysqlConnection.query("Select * from usuario where nombre = ?" , body.userName, (err, rows, field) => {
+    mysqlConnection.query("Select * from usuario where nombre = ?" , body.nombre, (err, rows, field) => {
         if (!err) {
             user = rows[0];
             if (user === undefined) {
                 return res.status(401).send('Usuario no Existe');
             }
-            if (body.password === user.password) {
+            if (body.contrasena === user.contrasena) {
                 const token = jwt.sign({_id: user.id }, 'secret', { expiresIn: '10m' });
-                return res.status(200).json({ token });
+                return res.status(200).json({token});
             } else {
                 return res.status(401).send('Invalido');
             }
