2930be50df55f92c45c3d1c374c29c2199f83a56
Humberto6641 <hgmezmartnez@gamil.com>
2021-11-07 19:16:39 -0600

    make it better

--- routes/security.js
+++ routes/security.js
@@ -7,17 +7,17 @@ const jwt = require('jsonwebtoken');
 //Visualizar Personas
 router.post("/login", (req, res) => {
     const body = req.body;
-    console.log(body.nombre); // nombre
+    console.log(body.userName); // nombre
     let user;    //user       
 
-    mysqlConnection.query("select * from usuario where nombre = 'humberto' and contrasena = '123456'" , body.nombre, (err, rows, field) => {
+    mysqlConnection.query("select * from usuario where nombre = 'humberto' and contrasena = '123456'" , body.userName, (err, rows, field) => {
         if (!err) {
             user = rows[0];
             if ( user === undefined) {
                 return res.status(401).send('Usuario no Existe');
             }
             if (body.contrasena === user.contrasena) { // contrasena
-                const token = jwt.sign({_id: _usuario.id }, 'secret', { expiresIn: '10m' });
+                const token = jwt.sign({_id: user.id }, 'secret', { expiresIn: '10m' });
                 return res.status(200).json({token});
             } else {
                 return res.status(401).send('Invalido');
